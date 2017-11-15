9281ee80456c35943803b03b273a183b04e47958
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-11-15 23:59:53 +0100

    Amending fields

--- src/app/fields/text-area/index.js
+++ src/app/fields/text-area/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import '../styles.css';
 import './styles.css';
 
 class TextArea extends React.Component {
+  getClassName(value) {
+    let className = "entry area";
+
+    if (value) {
+      className += " has-value"
+    }
+
+    return className;
+  }
+
   render() {
+    let { value, name } = this.props;
+    let className = this.getClassName(value);
+
     return (
       <div className="field text-area">
-        <label>{this.props.label}</label>
-
         <textarea
-          name={this.props.name}
-          id={this.props.name}
-          placeholder={this.props.placeholder}
+          id={name}
+          name={name}
+          value={value}
+          className={className}
+          onChange={this.props.onChange}
+          stateness={this.props.stateness}
         />
+
+        <label className="descr">
+          {this.props.label}
+        </label>
       </div>
     );
   }
--- src/app/fields/text/index.js
+++ src/app/fields/text/index.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import '../styles.css';
 import './styles.css';
 
 class Text extends React.Component {
+  getClassName(value) {
+    let className = "entry text";
+
+    if (value) {
+      className += " has-value"
+    }
+
+    return className;
+  }
+
   render() {
+    let { value, name } = this.props;
+    let className = this.getClassName(value);
+
     return (
       <div className="field text-area">
-        <label>{this.props.label}</label>
-
         <input
+          id={name}
           type="text"
-          name={this.props.name}
-          id={this.props.name}
-          placeholder={this.props.placeholder}
+          name={name}
+          value={value}
+          autoComplete="off"
+          className={className}
+          onChange={this.props.onChange}
+          stateness={this.props.stateness}
         />
+
+        <label className="descr">
+          {this.props.label}
+        </label>
       </div>
     );
   }
